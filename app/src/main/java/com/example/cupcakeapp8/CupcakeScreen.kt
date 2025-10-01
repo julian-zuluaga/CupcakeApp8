fd25d33d2155253e2fbc77b8e5371c01bb51e837
julian-zuluaga <julianzuluaga2008@gmail.com>
2025-10-01 13:24:16 -0500

    Configurar entrypoint de la app

---
+++ app/src/main/java/com/example/cupcakeapp8/CupcakeScreen.kt
@@ -0,0 +1,29 @@
+package com.example.cupcakeapp8
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cupcakeapp8.ui.theme.CupcakeApp8Theme
+
+@Composable
+fun CupcakeApp8() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
