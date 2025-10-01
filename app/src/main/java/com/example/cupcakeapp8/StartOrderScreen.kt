e58c995d3d7678111299a477d609c89a1b559820
julian-zuluaga <julianzuluaga2008@gmail.com>
2025-10-01 13:40:39 -0500

    Añadir imagen de cupcake con espaciado

--- app/src/main/java/com/example/cupcakeapp8/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp8/StartOrderScreen.kt
@@ -10,6 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.dimensionResource
 import com.example.cupcakeapp8.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
 
 
 @Composable
@@ -23,11 +30,17 @@ fun StartOrderScreen(
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(
-                dimensionResource(R.dimen.padding_small)
-            )
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
         ) {
-            // Aquí irán los elementos de la parte superior
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+
+            Image(
+                painter = painterResource(R.drawable.cupcake),
+                contentDescription = null,
+                modifier = Modifier.width(300.dp)
+            )
+
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
         }
     }
 }
