4d379adcd4d1156467ab922cbf794e9b0e25f57f
julian-zuluaga <julianzuluaga2008@gmail.com>
2025-10-01 13:59:25 -0500

    Agregar DataSource con opciones de cantidad

---
+++ app/src/main/java/com/example/cupcakeapp8/data/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.cupcakeapp8.data
+
+import com.example.cupcakeapp8.R
+
+object DataSource {
+    val quantityOptions = listOf(
+        Pair(R.string.one_cupcake, 1),
+        Pair(R.string.six_cupcakes, 6),
+        Pair(R.string.twelve_cupcakes, 12)
+    )
+}
--- app/src/main/java/com/example/cupcakeapp8/ui/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp8/ui/StartOrderScreen.kt
@@ -1,4 +1,4 @@
-package com.example.cupcakeapp8
+package com.example.cupcakeapp8.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
